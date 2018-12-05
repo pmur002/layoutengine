9808e46a86c70ca2001756ee674d07d711c164a1
Paul Murrell <paul@stat.auckland.ac.nz>
2018-12-06 12:10:03 +1300

    improve regression test script

--- inst/regression-tests/test.R
+++ inst/regression-tests/test.R
@@ -117,20 +117,35 @@ grid.rect()
 dev.off()
 
 ## Check graphical output
-savedPDF <- system.file("regression-tests", "tests.save.pdf",
-                        package="layoutEngine")
-diff <- tools::Rdiff("tests.pdf", savedPDF)
-
-if (diff != 0L) {
-    ## If differences found, generate images of the differences and error out
-    system("pdfseparate tests.pdf test-pages-%d.pdf")
+testoutput <- function(basename) {
+    PDF <- paste0(basename, ".pdf")
+    savedPDF <- system.file("regression-tests", paste0(basename, ".save.pdf"),
+                            package="layoutEngine")
+    system(paste0("pdfseparate ", PDF, " test-pages-%d.pdf"))
     system(paste0("pdfseparate ", savedPDF, " model-pages-%d.pdf"))
-    modelFiles <- list.files(pattern="model-pages-.*")
+    modelFiles <- list.files(pattern="model-pages-.*[.]pdf")
     N <- length(modelFiles)
+    allGood <- TRUE
     for (i in 1:N) {
-        system(paste0("compare model-pages-", i, ".pdf ",
+        system(paste0("convert -density 96 ",
+                      "model-pages-", i, ".pdf ",
+                      "model-pages-", i, ".png"))
+        system(paste0("convert -density 96 ",
                       "test-pages-", i, ".pdf ",
-                      "diff-pages-", i, ".png"))
-    } 
-    stop("Regression testing detected differences")
+                      "test-pages-", i, ".png"))
+        result <- system(paste0("compare -metric AE ",
+                                "model-pages-", i, ".png ",
+                                "test-pages-", i, ".png ",
+                                "diff-pages-", i, ".png ",
+                                "2>&1"), intern=TRUE)
+        if (result != "0") {
+            cat(paste0("Test and model differ (page ", i, "; ",
+                       "see diff-pages-", i, ".png)\n"))
+            allGood <- FALSE
+        }
+    }
+    if (!allGood)
+        stop("Regression testing detected differences")
 }
+
+testoutput("tests")
